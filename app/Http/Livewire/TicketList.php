37f09462892116046d38a253353ac9f4770c821d
Christopher McComas <cmccomas@marshall.edu>
2020-10-29 12:36:01 -0400

    wip

--- app/Http/Livewire/TicketList.php
+++ app/Http/Livewire/TicketList.php
@@ -37,25 +37,124 @@ class TicketList extends Component
 
         while ( ( $data = fgetcsv($file)) !== FALSE)
         {
-            if (trim($data[1]) !== 'CustPri' ) {
+            if (trim($data[0]) !== 'Donor' ) {
 
+                $balls = 0;
                 $rank++;
+
                 $customer = trim($data[0]);
-                $points = trim($data[1]);
+                $name = trim($data[1]);
+                $phone = trim($data[2]);
+                $email = trim($data[3]);
+
+                $points = trim($data[4]);
                 $points = str_replace(',', '', $points);
                 $points = floatval($points);
 
+                $af20 = trim($data[5]);
+                $haf = trim($data[6]);
+                $tipoff = trim($data[7]);
+                $rises = trim($data[8]);
+
+                // Calculate Priority Point Balls
+                // $ppBalls
                 if ($points <= 100) {
-                    $weight = 1;
+                    $ppBalls = 1;
                 } else {
-                    $weight = round($points) / 100;
+                    $ppBalls = round($points) / 100;
                 }
 
-                for ($x = 0; $x <= $weight; $x++) {
+                // Calculate Annual Fund 2020 Balls
+                // $afBalls
+                if ($af20 >= 60 && $af20 <= 299) {
+                    $afBalls = 1;
+                } else if ($af20 >= 300 && $af20 <= 999) {
+                    $afBalls = 2;
+                } else if ($af20 >= 1000 && $af20 <= 1499) {
+                    $afBalls = 3;
+                } else if ($af20 >= 1500 && $af20 <= 4999) {
+                    $afBalls = 5;
+                } else if ($af20 >= 5000 && $af20 <= 9999) {
+                    $afBalls = 15;
+                } else if ($af20 >= 10000) {
+                    $afBalls = 30;
+                } else {
+                    $afBalls = 0;
+                }
+
+                // Calculate HAF Balls
+                // $hafBalls
+                // $2,500 – 5 entries
+                // $5,000 – 10 entries
+                // $10,000 – 25 entries
+                if ($haf >= 2500 && $haf <= 4999) {
+                    $hafBalls = 5;
+                } else if ($haf >= 5000 && $haf <= 9999) {
+                    $hafBalls = 10;
+                } else if ($haf >= 5000) {
+                    $hafBalls = 25;
+                } else {
+                    $hafBalls = 0;
+                }
+
+                // Calculate Herd Rises Balls
+                // $risesBalls
+                // $1,000 - $5,000 – 5 entries
+                // $5,001 - $10,000 – 10 entries
+                // $10,001 - $25,000 – 20 entries
+                // $25,001 - $99,999 – 30 entries
+                // $100,000 + - 50 entries
+                if ($rises >= 1000 && $rises <= 5000) {
+                    $risesBalls = 5;
+                } else if ($rises >= 5001 && $rises <= 10000) {
+                    $risesBalls = 10;
+                } else if ($rises >= 10001 && $rises <= 25000) {
+                    $risesBalls = 20;
+                } else if ($rises >= 25001 && $rises <= 99999) {
+                    $risesBalls = 30;
+                } else if ($rises >= 10000) {
+                    $risesBalls = 50;
+                } else {
+                    $risesBalls = 0;
+                }
+
+                // Calculate Tip Off Club Balls
+                // $tipoffBalls
+                // $150 – 1 entry
+                // $300 – 2 entries
+                // $600 – 3 entries
+                // $1,500 – 4 entries
+                // $5,000 – 5 entries
+                if ($tipoff >= 150 && $tipoff <= 299) {
+                    $tipoffBalls = 1;
+                } else if ($tipoff >= 300 && $tipoff <= 599) {
+                    $tipoffBalls = 2;
+                } else if ($tipoff >= 600 && $tipoff <= 1499) {
+                    $tipoffBalls = 3;
+                } else if ($tipoff >= 1500 && $tipoff <= 4999) {
+                    $tipoffBalls = 4;
+                } else if ($tipoff >= 5000) {
+                    $tipoffBalls = 5;
+                } else {
+                    $tipoffBalls = 0;
+                }
+
+                // $ppBalls + $afBalls + $hafBalls + tipoffBalls;
+                $balls = $ppBalls + $afBalls + $hafBalls + $tipoffBalls + $risesBalls;
+
+                for ($x = 0; $x <= $balls; $x++) {
                     Customer::create([
                         'customer_id' => $customer,
+                        'name' => $name,
+                        'phone' => $phone,
+                        'email' => $email,
                         'points' => $points,
-                        'rank' => $rank,
+                        'annual_fund' => $af20,
+                        'herd_athletic_fund' => $haf ? $haf : 0,
+                        'herd_rises' => $rises,
+                        'tip_off_club' => $tipoff,
+                        'balls' => $balls,
+                        'rank' => $rank
                     ]);
                 }
             }
@@ -76,12 +175,12 @@ class TicketList extends Component
         $entries = Customer::inRandomOrder()->get();
         $output = "";
 
-        $output = "CustomerNumber,CustPri,Rank\r\n";
+        $output = "Donor,Name,C Phone,E Phone,Prior,Drv 20 Commit,HAF 19 & 20,RB19 TO usage commit,Usage beginning with HR Commit all drives,Balls\r\n";
 
         foreach ($entries as $entry) {
             if (!in_array($entry->customer_id, $this->outputList)) {
                 array_push($this->outputList, $entry->customer_id);
-                $output .= "\"" . $entry->customer_id . "\",\"" . $entry->points . "\",\"" . $entry->rank . "\"\r\n";
+                $output .= "\"" . $entry->customer_id . "\",\"" . $entry->name . "\",\"" . $entry->phone . "\",\"" . $entry->email . "\",\"" . $entry->points . "\",\"" . $entry->annual_fund . "\",\"" . $entry->herd_athletic_fund . "\",\"" . $entry->tip_off_club . "\",\"" . $entry->herd_rises . "\",\"" . $entry->balls . "\"\r\n";
             }
         }
 
--- database/migrations/2020_10_22_120304_create_customers_table.php
+++ database/migrations/2020_10_22_120304_create_customers_table.php
@@ -16,11 +16,17 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('customer_id');
-            $table->float('points', 12, 4);
-            $table->integer('weight')->nullable();
             $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->float('points', 12, 4);
+            $table->float('annual_fund', 12, 4);
+            $table->integer('herd_athletic_fund')->default(0);
+            $table->integer('tip_off_club')->default(0);
+            $table->string('herd_rises')->default(0);
             $table->integer('tickets_alloted')->nullable();
             $table->integer('rank');
+            $table->integer('balls')->default(0);
             $table->timestamps();
         });
     }
